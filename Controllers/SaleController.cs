e665bd678a15bacacd229b66852ac2be0c0b064e
Alan Nogueira <alan.nogueira991@gmail.com>
2023-03-01 22:43:47 -0300

    Adicionado controllers e views

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Inventory_Control.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Inventory_Control.Controllers
+{
+    public class SaleController : Controller
+    {
+        private readonly InventoryControlContext _context;
+
+        public SaleController(InventoryControlContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Controllers/SellerController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Inventory_Control.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Inventory_Control.Controllers
+{
+    public class SellerController : Controller
+    {
+        private readonly InventoryControlContext _context;
+
+        public SellerController(InventoryControlContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
